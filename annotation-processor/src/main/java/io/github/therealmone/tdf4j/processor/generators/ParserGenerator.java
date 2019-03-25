e48fc4dcbd9a2a2d7933b12f692d98bb24edffcc
therealmone <r.fatnev@gmail.com>
2019-03-25 17:12:53 +0300

    Validation, process-classes phase

--- annotation-processor/src/main/java/io/github/therealmone/tdf4j/processor/generators/ParserGenerator.java
+++ annotation-processor/src/main/java/io/github/therealmone/tdf4j/processor/generators/ParserGenerator.java
@@ -1,12 +1,41 @@
 package io.github.therealmone.tdf4j.processor.generators;
 
+import io.github.therealmone.tdf4j.parser.config.AbstractParserModule;
+
 import javax.annotation.processing.ProcessingEnvironment;
-import javax.lang.model.element.Element;
+import javax.lang.model.element.*;
+import javax.lang.model.type.TypeMirror;
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
 
 public class ParserGenerator implements Generator {
     @Override
-    public void generate(ProcessingEnvironment processingEnv, Element element) throws Exception {
+    public void generate(final ProcessingEnvironment processingEnv, final Element element) throws Exception {
         //todo: Вся логика генерации парсеров
-        processingEnv.getFiler().createClassFile(element.getSimpleName() + "Parser");
+        validate(processingEnv, element);
+        final JavaFileObject javaFileObject = processingEnv.getFiler().createClassFile(element.getSimpleName() + "Parser");
+        final Class clazz = Class.forName(element.toString());
+        final AbstractParserModule abstractParserModule = ((AbstractParserModule) clazz.newInstance()).build();
+        System.out.println(abstractParserModule.getProductions());
+    }
+
+    private void validate(final ProcessingEnvironment processingEnv, final Element element) {
+        if(element.getKind() != ElementKind.CLASS) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Only classes can be annotated with @Parser");
+        }
+
+        final TypeElement typeElement = (TypeElement) element;
+        if(!typeElement.getModifiers().contains(Modifier.PUBLIC)) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Annotated with @Parser class must be public");
+        }
+
+        if(typeElement.getModifiers().contains(Modifier.ABSTRACT)) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Annotated with @Parser class can't be abstract");
+        }
+
+        final TypeMirror superClass = processingEnv.getElementUtils().getTypeElement(AbstractParserModule.class.getCanonicalName()).asType();
+        if(!typeElement.getSuperclass().toString().equalsIgnoreCase(superClass.toString())) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Annotated with @Parser class must extend AbstractParserModule");
+        }
     }
 }
