597dc5127ca6019d918a32f6deb626bbbf6b0672
therealmone <r.fatnev@gmail.com>
2019-04-22 16:04:36 +0300

    Hidden terminals test

---
+++ lexer/src/test/java/io/github/therealmone/tdf4j/lexer/HiddenTerminalsTest.java
@@ -0,0 +1,55 @@
+package io.github.therealmone.tdf4j.lexer;
+
+import io.github.therealmone.tdf4j.commons.Token;
+import io.github.therealmone.tdf4j.lexer.config.AbstractLexerModule;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class HiddenTerminalsTest {
+
+    @Test
+    public void white_spaces() {
+        final Lexer lexer = new LexerFactory().withModule(new AbstractLexerModule() {
+            @Override
+            public void configure() {
+                tokenize("ws").pattern("\\s|\\n|\\r").hidden(true);
+            }
+        });
+        assertEquals(0, lexer.analyze("   \n  \r \n\r  \r\n").size());
+    }
+
+    @Test
+    public void single_line_comment() {
+        final Lexer lexer = new LexerFactory().withModule(new AbstractLexerModule() {
+            @Override
+            public void configure() {
+                tokenize("single_line_comment").pattern("//.*(\n|\r|\r\n|\n\r)").hidden(true);
+                tokenize("VAR").pattern("[A-Z]+");
+            }
+        });
+        final List<Token> tokens = lexer.analyze("//comment\nA//comment\rB//comment\r\nC");
+        assertEquals(3, tokens.size());
+        assertEquals("A", tokens.get(0).value());
+        assertEquals("B", tokens.get(1).value());
+        assertEquals("C", tokens.get(2).value());
+    }
+
+    @Test
+    public void multi_line_comment() {
+        final Lexer lexer = new LexerFactory().withModule(new AbstractLexerModule() {
+            @Override
+            public void configure() {
+                tokenize("multi_line_comment").pattern("/\\*[^(\\*/)]*\\*/").hidden(true);
+                tokenize("VAR").pattern("[A-Z]+");
+            }
+        });
+        final List<Token> tokens = lexer.analyze("/*comment\n\r\n\n*/A/*comment*/B/*\ncomment\n*/C");
+        assertEquals(3, tokens.size());
+        assertEquals("A", tokens.get(0).value());
+        assertEquals("B", tokens.get(1).value());
+        assertEquals("C", tokens.get(2).value());
+    }
+}
