d0ce4a3fe9d1d8be26a1bda60a98fe9b133d400c
therealmonE <r.fatnev@gmail.com>
2019-05-18 23:52:11 +0300

    AST refactoring

---
+++ parser/src/main/java/io/github/therealmone/tdf4j/parser/model/ast/ASTImpl.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2019 Roman Fatnev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.therealmone.tdf4j.parser.model.ast;
+
+import io.github.therealmone.tdf4j.commons.Token;
+
+import java.util.function.Consumer;
+
+class ASTImpl implements AST {
+    private final ASTRoot root;
+    private final ASTCursor cursor;
+
+    ASTImpl(final ASTRoot root) {
+        this.root = root;
+        this.cursor = new ASTCursor(this.root);
+    }
+
+    @Override
+    public AST addNode(final String tag) {
+        return addNode(ModifiableASTNode.create()
+                .setTag(tag)
+                .setParent(cursor.getValue())
+        );
+    }
+
+    @Override
+    public AST addNode(final ASTNode node) {
+        addChild(cursor, node);
+        return this;
+    }
+
+    @Override
+    public AST addLeaf(final Token token) {
+        return addLeaf(ModifiableASTLeaf.create()
+                .setToken(token)
+                .setParent(cursor.getValue())
+        );
+    }
+
+    @Override
+    public AST addLeaf(final ASTLeaf leaf) {
+        addChild(cursor, leaf);
+        return this;
+    }
+
+    @Override
+    public ASTImpl moveCursor(final Consumer<ASTCursor> movement) {
+        movement.accept(cursor);
+        return this;
+    }
+
+    @Override
+    public ASTImpl moveCursor(final ASTCursor.Movement movement) {
+        movement.accept(cursor);
+        return this;
+    }
+
+    private void addChild(final ASTCursor cursor, final ASTElement element) {
+        if(cursor.isNode()) {
+            cursor.asNode().children().add(element);
+        } else if(cursor.isRoot()) {
+            cursor.asRoot().children().add(element);
+        }
+    }
+
+    @Override
+    public ASTRoot getRoot() {
+        return root;
+    }
+
+    @Override
+    public ASTElement onCursor() {
+        return cursor.getValue();
+    }
+
+    @Override
+    public String toString() {
+        return root.toString();
+    }
+}
