1dc998a7b49cba1fdb51dfee451546e248323da4
therealmone <r.fatnev@gmail.com>
2019-05-29 18:33:06 +0300

    .tdf parser module

---
+++ tdf-parser/src/main/java/io/github/therealmone/tdf4j/tdfparser/LexerModule.java
@@ -0,0 +1,12 @@
+package io.github.therealmone.tdf4j.tdfparser;
+
+import io.github.therealmone.tdf4j.module.lexer.AbstractLexerModule;
+
+public class LexerModule extends AbstractLexerModule {
+
+    @Override
+    public void configure() {
+
+    }
+
+}
